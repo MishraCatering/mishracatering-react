f64a6669047d591c32b83ef49c7b6f4b5d4e8835
Suraj Kumar <i.suraj06.kumar@gmail.com>
2025-09-09 21:06:23 +0530

    Packages API integrated

--- src/app/components/OurServicecard.js
+++ src/app/components/OurServicecard.js
@@ -1,3 +1,5 @@
+'use client';
+import { useEffect, useState } from 'react';
 import Link from "next/link";
 import './ourservicecard.css';
 import { FaLongArrowAltRight } from "react-icons/fa";
@@ -5,90 +7,62 @@ import menucateringimg from '../../../public/images/catering-image.png';
 import servcmenuicon from '../../../public/images/menu-icon.png';
 import Image from "next/image";
 import { TfiMenuAlt } from "react-icons/tfi";
+import { baseURL } from "../lib/constants";
 
-const OurServicecard = ({categories}) =>{
-    return(
+const OurServicecard = ({ categories }) => {
+    const [packages, setpackages] = useState([]);
+
+    async function loadPackages() {
+        const res = await fetch(`${baseURL}/packages/api/packages/`)
+            .then(response => response.json())
+            .then((jsonData) => {
+                setpackages(jsonData)
+            })
+    }
+
+    useEffect(() => {
+        loadPackages()
+    }, [])
+    return (
         <>
-        <div className="servc-card-block">
-            {categories.map((item, index) => {
-                return (
-                    <div className="card-tab" key={index}>
-                        <Link href="">{item.name}</Link>
-                    </div>
-                )
-            })}
-        </div>
-        <div className="servc-menu-card-blk">
-            <div className="servc-menu-card">
-                <div className="servc-menu">
-                    <h4>Mishra Box Plan</h4>
-                    <div className="servc-menu-iconwth-tlt">
-                        <Image src={servcmenuicon} alt="menu-icon" />
-                        <p>Ideal for 10-25 Guest</p>
-                    </div>
-                    <div className="servc-menu-iconwth-list">
-                        <ul>
-                            <li><FaLongArrowAltRight /> No Mess in Kitchen</li>
-                            <li><FaLongArrowAltRight /> No Cleaning Required</li>
-                            <li><FaLongArrowAltRight /> Includes Displosals</li>
-                        </ul>
-                    </div>
-                </div>
-                
-                <div className="servc-menu-cta">
-                    <Link href="#">See Packages <TfiMenuAlt /> </Link>
-                </div>
-                
-                    <Image className="serv-menu-img" src={menucateringimg} alt="catering service" />
-                
+            <div className="servc-card-block">
+                {categories.map((item, index) => {
+                    return (
+                        <div className="card-tab" key={index}>
+                            <Link href="">{item.name}</Link>
+                        </div>
+                    )
+                })}
             </div>
-            <div className="servc-menu-card">
-            <div className="servc-menu">
-                    <h4>Mishra Box Plan</h4>
-                    <div className="servc-menu-iconwth-tlt">
-                        <Image src={servcmenuicon} alt="menu-icon" />
-                        <p>Ideal for 10-25 Guest</p>
-                    </div>
-                    <div className="servc-menu-iconwth-list">
-                        <ul>
-                            <li><FaLongArrowAltRight /> No Mess in Kitchen</li>
-                            <li><FaLongArrowAltRight /> No Cleaning Required</li>
-                            <li><FaLongArrowAltRight /> Includes Displosals</li>
-                        </ul>
-                    </div>
-                </div>
-                
-                <div className="servc-menu-cta">
-                    <Link href="#">See Packages <TfiMenuAlt /> </Link>
-                </div>
-                <div className="servc-menu-img">
-                    <Image className="serv-menu-img" src={menucateringimg} alt="catering service" />
-                </div>
+            <div className="servc-menu-card-blk">
+                {packages.map((item, index) => {
+                    return (
+                        <div className="servc-menu-card" key={index}>
+                            <div className="servc-menu">
+                                <h4>{item.name}</h4>
+                                <div className="servc-menu-iconwth-tlt">
+                                    <Image src={servcmenuicon} alt="menu-icon" />
+                                    <p>{item.size_description}</p>
+                                </div>
+                                <div className="servc-menu-iconwth-list">
+                                    <ul>
+                                        <li><FaLongArrowAltRight /> {item.line_1}</li>
+                                        <li><FaLongArrowAltRight /> {item.line_2}</li>
+                                        <li><FaLongArrowAltRight /> {item.line_3}</li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div className="servc-menu-cta">
+                                <Link href="#">See Packages <TfiMenuAlt /> </Link>
+                            </div>
+
+                            <Image className="serv-menu-img" src={menucateringimg} alt="catering service" />
+
+                        </div>
+                    )
+                })}
             </div>
-            <div className="servc-menu-card">
-            <div className="servc-menu">
-                    <h4>Mishra Box Plan</h4>
-                    <div className="servc-menu-iconwth-tlt">
-                        <Image src={servcmenuicon} alt="menu-icon" />
-                        <p>Ideal for 10-25 Guest</p>
-                    </div>
-                    <div className="servc-menu-iconwth-list">
-                        <ul>
-                            <li><FaLongArrowAltRight /> No Mess in Kitchen</li>
-                            <li><FaLongArrowAltRight /> No Cleaning Required</li>
-                            <li><FaLongArrowAltRight /> Includes Displosals</li>
-                        </ul>
-                    </div>
-                </div>
-                
-                <div className="servc-menu-cta">
-                    <Link href="#">See Packages <TfiMenuAlt /> </Link>
-                </div>
-                <div className="servc-menu-img">
-                    <Image className="serv-menu-img" src={menucateringimg} alt="catering service" />
-                </div>
-            </div>
-        </div>
         </>
     )
 }
